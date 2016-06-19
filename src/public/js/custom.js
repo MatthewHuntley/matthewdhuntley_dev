3b15cf839b09424e1fefd77013f612a366adad1c
Matthew Huntley <matthewhuntley@Matthews-MacBook-Pro.local>
2016-06-19 11:14:43 -0400

    Committing all new and modified files 06-19-16.

---
+++ src/public/js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('.navbar-header').click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click()
+            .css('background-color', 'background-color: rgba(30,33,31,1)');
+        }
+    });
+
+    //START HERE!
+    $(".navbar-toggle").click(function() {
+    	var clickover = $(event.target);
+    	if(!($('.navbar-toggle').hasClass("navbar-toggle"))) {
+    		$(".navbar-toggle").addClass('blah');
+    		$(".blah").click(function() {
+		    	console.log("Hello");
+		    	$(".navbar-toggle").removeClass('blah');
+		    });
+    	}
+    });
+
+
+
+  //   if($(".navbar-collapse").hasClass("navbar-collapse collapse in")) {
+		
+		// $('.navbar-toggle').click(function (event) {
+		//     		console.log("Hello");
+		// 		});
+
+
+  //   	//$("button.navbar-toggle").css('background-color', 'background-color: rgba(30,33,31,1)');    });
+  //   }
+
+  //   $('.in').click(function() {
+  //   	console.log("Hello");
+  //   });
+
+});
