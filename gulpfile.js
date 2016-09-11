e75ed78c38ec78fbd3a6540275fbc931215ba58a
Matthew Huntley <matthewhuntley@Matthews-MacBook-Pro.local>
2016-09-11 10:41:51 -0400

    Committing all files added/modified 09-11-16.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,23 @@ var gulp = require('gulp'),
  	  //maps = require('gulp-sourcemaps'),
  	   //del = require('del');
 
-gulp.task('concatScripts', function() {
+gulp.task('concatCSSFiles', function() {
+	gulp.src([	
+		'src/public/css/bootstrap.css', 
+		'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800',
+		'http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic',
+		'src/public/font-awesome/css/font-awesome.min.css', 
+		'src/public/css/animate.min.css', 
+		'src/public/js/slick/slick.css', 
+		'src/public/js/slick/slick-theme.css',
+		'src/public/css/styles.css',
+		'src/public/css/index.css',
+		'src/public/css/responsive.css'])
+	.pipe(concat('main.css'))
+	.pipe(gulp.dest('src/public/css/'));
+});
+
+gulp.task('concatJSFiles', function() {
 	gulp.src([	
 		'src/public/js/jquery.js', 
 		'src/public/js/bootstrap.min.js', 
@@ -23,6 +39,6 @@ gulp.task('concatScripts', function() {
 
 //inherent 'gulp' task (named when 'default' is the name of the task) first runs the other gulp dependencies
 //Then run 'build' task automatically via inherent gulp.start() method (start saves us the trouble of typing out "gulp build", which is being deprecated)
-gulp.task('default', function() {
-	console.log("Hello");
+gulp.task('default', ['concatCSSFiles', 'concatJSFiles'], function() {
+	console.log("External files concatenated.");
 });
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -139,6 +139,15 @@ $(document).ready(function () {
     });
     $(".active").parents(".navbar li").addClass("active-top");
 
+    //Slick carousel
+    $('.wrapper-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+
     //START HERE!
     // $(".navbar-toggle").click(function() {
     // 	var clickover = $(event.target);
